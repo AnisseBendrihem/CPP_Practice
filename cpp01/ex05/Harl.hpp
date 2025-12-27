320c37f71fa3a1aea66a835e1fe59383e5f2c261
Anisse Bendrihem <anisse.bendrihem@gmail.com>
2025-12-27 15:52:10 +0100

    cpp01/ex05 understood

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abendrih <abendrih@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/27 14:54:20 by abendrih          #+#    #+#             */
+/*   Updated: 2025/12/27 15:47:33 by abendrih         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+
+class Harl
+{
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+
+    public:
+        Harl();
+        ~Harl();
+        void complain( std::string level);
+};
+
+
+#endif
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abendrih <abendrih@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/27 14:54:25 by abendrih          #+#    #+#             */
+/*   Updated: 2025/12/27 15:47:33 by abendrih         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+    
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    
+    return 0;
+}
